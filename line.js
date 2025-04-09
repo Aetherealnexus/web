0eca64a4963001953b53419d903fc081f7affce2
xryv <bru.cerqueira5@gmail.com>
2025-04-09 10:04:20 +0100

    Added line.css and line.js

---
+++ line.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+    // Optionally trigger the animation with a delay if needed
+    setTimeout(() => {
+      document.querySelector('.animated-line').classList.add('visible');
+    }, 500); // 500ms delay before line appears
+  });
+  
